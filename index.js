9edff685618fd09eb47d9ab25602509e05d7be7f
joe lazarz <jlazarz87@gmail.com>
2019-08-06 12:01:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const testVar = "hi"
-
+// removes DOM node main#main
 document.querySelector("main#main").remove();
-
+// creates new h1 element
 const newHeader = document.createElement("h1");
+// sets newHeader id to "victory"
 newHeader.id = "victory"
+// sets the innerHTML of newHeader
 newHeader.innerHTML = "Flatiron School is the champion!";
+// sets class of newHeader
 newHeader.className = "victory";
