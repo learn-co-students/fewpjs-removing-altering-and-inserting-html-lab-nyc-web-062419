e6c31ae619974674c902e3c66e670e92dae9a3ba
thenatural86 <philip.smalls86@gmail.com>
2019-08-06 09:00:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+const yo = 'yo';
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Philip Smalls is the champion.';
