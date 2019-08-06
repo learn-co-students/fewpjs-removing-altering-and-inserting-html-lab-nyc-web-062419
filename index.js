90ed67c205beb2cfbeea6058c20bd037b434ed8a
yoseflehrer <yolehrer@gmail.com>
2019-08-06 11:11:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+newHeader.id = "victory"
+let main = document.getElementById("main");
+main.remove()
+
+
+function addElement () { 
+    // create a new div element 
+    var newHeader = document.createElement("h1"); 
+    // and give it some content 
+    var newContent = document.createTextNode("Hi there and greetings!"); 
+    // add the text node to the newly created div
+    newHeader.appendChild(newContent);  
+  
+    // add the newly created element and its content into the DOM 
+    document.body.append(newDiv); 
+  }
