f6db2288b18655b1736fe479774f10ff1b15cd65
KConlon13 <KyleConlon13@gmail.com>
2019-08-05 21:14:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Kyle is the champion";
+newHeader.className = "victory";
